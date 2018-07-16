b0c85576c056a88e9ba297a010a5a1b408cb5cd9
Hossain Khademian <hco@post.com>
2018-07-16 10:58:25 +0430

    add FieldBox action1 action2 and action3

--- app/src/main/java/ir/hossainkhademian/jobs/view/FieldBox.kt
+++ app/src/main/java/ir/hossainkhademian/jobs/view/FieldBox.kt
@@ -11,7 +11,9 @@ import kotlinx.android.synthetic.main.view_field_box.view.*
 class FieldBox : CardView {
   private var view: View? = null
   private val titleView get() = view!!.titleView
-  private val actionView get() = view!!.actionView
+  private val action1View get() = view!!.action1View
+  private val action2View get() = view!!.action2View
+  private val action3View get() = view!!.action3View
   private val childView get() = view!!.childView
 
   constructor(context: Context) : super(context) {
@@ -24,7 +26,9 @@ class FieldBox : CardView {
 
     val a = context.obtainStyledAttributes(attrs, R.styleable.FieldBox, defStyleAttr, defStyleAttr)
     title = a.getString(R.styleable.FieldBox_android_text) ?: ""
-    action = a.getString(R.styleable.FieldBox_action) ?: ""
+    action1 = a.getString(R.styleable.FieldBox_action1) ?: a.getString(R.styleable.FieldBox_action) ?: ""
+    action2 = a.getString(R.styleable.FieldBox_action2) ?: ""
+    action3 = a.getString(R.styleable.FieldBox_action3) ?: ""
     a.recycle()
   }
 
@@ -32,7 +36,11 @@ class FieldBox : CardView {
     view = inflate(context, R.layout.view_field_box, this)
     title = ""
     action = ""
-    onActionClickListener = null
+    action2 = ""
+    action3 = ""
+    onAction1ClickListener = null
+    onAction2ClickListener = null
+    onAction3ClickListener = null
 
     radius = 8 * context.resources.displayMetrics.scaledDensity
   }
@@ -49,16 +57,53 @@ class FieldBox : CardView {
     }
 
   var action: String
-    get() = actionView.text.toString()
+    get() = action1
+    set(value) = let { action1 = value }
+
+
+  var action1: String
+    get() = action1View.text.toString()
     set(value) {
-      actionView.text = value
-      actionView.visibility = if (value.isEmpty()) View.GONE else View.VISIBLE // use space to not gone
+      action1View.text = value
+      action1View.visibility = if (value.isEmpty()) View.GONE else View.VISIBLE // use space to not gone
     }
 
-  var onActionClickListener: View.OnClickListener? = null // View.OnClickListener { }
+  var action2: String
+    get() = action2View.text.toString()
+    set(value) {
+      action2View.text = value
+      action2View.visibility = if (value.isEmpty()) View.GONE else View.VISIBLE // use space to not gone
+    }
+
+  var action3: String
+    get() = action3View.text.toString()
+    set(value) {
+      action3View.text = value
+      action3View.visibility = if (value.isEmpty()) View.GONE else View.VISIBLE // use space to not gone
+    }
+
+  var onActionClickListener: View.OnClickListener?
+    get() = onAction1ClickListener
+    set(listener) = let { onAction1ClickListener = listener }
+
+  var onAction1ClickListener: View.OnClickListener? = null // View.OnClickListener { }
     set(listener) {
       field = listener
-      actionView.setOnClickListener(listener)
-      actionView.isEnabled = listener != null // use empty listener if you want
+      action1View.setOnClickListener(listener)
+      action1View.isEnabled = listener != null // use empty listener if you want
+    }
+
+  var onAction2ClickListener: View.OnClickListener? = null // View.OnClickListener { }
+    set(listener) {
+      field = listener
+      action2View.setOnClickListener(listener)
+      action2View.isEnabled = listener != null // use empty listener if you want
+    }
+
+  var onAction3ClickListener: View.OnClickListener? = null // View.OnClickListener { }
+    set(listener) {
+      field = listener
+      action3View.setOnClickListener(listener)
+      action3View.isEnabled = listener != null // use empty listener if you want
     }
 }
