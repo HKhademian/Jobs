c42a2c797a14aade3dae7a0734ef37302248221c
Hossain Khademian <hco@post.com>
2018-07-17 22:33:57 +0430

    correct large image files

--- test/src/main/java/com/example/test/ScrollingActivity.kt
+++ test/src/main/java/com/example/test/ScrollingActivity.kt
@@ -3,8 +3,6 @@ package com.example.test
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_scrolling.*
 
 class ScrollingActivity : AppCompatActivity() {
@@ -16,23 +14,6 @@ class ScrollingActivity : AppCompatActivity() {
     fab.setOnClickListener { view ->
       Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
         .setAction("Action", null).show()
-    }
-  }
-
-  override fun onCreateOptionsMenu(menu: Menu): Boolean {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    menuInflater.inflate(R.menu.menu_scrolling, menu)
-    return true
-  }
-
-  override fun onOptionsItemSelected(item: MenuItem): Boolean {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-
-    return when (item.itemId) {
-      R.id.action_settings -> true
-      else -> super.onOptionsItemSelected(item)
     }
   }
 }
